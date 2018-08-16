3dca656e279a05b3df8b7ee5116f883dc6830b5f
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-08-16 10:59:29 +0530

    Configured devise with carrearwave gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :name])
+
 end
