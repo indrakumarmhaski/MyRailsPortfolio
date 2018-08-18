d88f240bdcd060e84845bd920b06448b8015baac
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-08-18 13:42:00 +0530

    Configured the petergate gem with devise gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_030119) do
+ActiveRecord::Schema.define(version: 2018_08_18_080829) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2018_08_17_030119) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.string "name"
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
