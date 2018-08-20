36afeadaac2e9b2ec3d59e62d2b56d4e615743c2
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-08-20 11:10:19 +0530

    Added tags feature for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
   mount_uploader :blogimage, BlogimageUploader
+  has_many :tags, dependent: :destroy, inverse_of: :blog
+  accepts_nested_attributes_for :tags,
+                                      reject_if: lambda { |attrs| attrs['topic'].blank? },
+                                      allow_destroy: true
 end
