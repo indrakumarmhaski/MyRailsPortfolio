463899cd928a25d5134d791577574b151a28f92e
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-08-24 18:48:52 +0530

    Configured workexample model to accept nested attributes

--- app/models/workexample.rb
+++ app/models/workexample.rb
@@ -2,5 +2,8 @@ class Workexample < ApplicationRecord
   belongs_to :user
   mount_uploader :thumbimage, ThumbimageUploader
   mount_uploader :mainimage, MainimageUploader
-  has_many :technologies, dependent: :destroy
+  has_many :technologies, dependent: :destroy, inverse_of: :workexample
+  accepts_nested_attributes_for :technologies,
+                                      reject_if: lambda { |attrs| attrs['topic'].blank? },
+                                      allow_destroy: true
 end
