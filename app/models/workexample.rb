25095653a3a4572fcb9d063376f5ad14e1c34747
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-08-24 18:45:48 +0530

    Stablished relations

--- app/models/workexample.rb
+++ app/models/workexample.rb
@@ -2,4 +2,5 @@ class Workexample < ApplicationRecord
   belongs_to :user
   mount_uploader :thumbimage, ThumbimageUploader
   mount_uploader :mainimage, MainimageUploader
+  has_many :technologies, dependent: :destroy
 end
