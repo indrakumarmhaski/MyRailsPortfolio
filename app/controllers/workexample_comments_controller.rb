a746ee09909708340e7f412dade464a1bd966755
indrakumarmhaski <indrakumarmhaski@gamil.com>
2018-08-24 20:29:24 +0530

    Configured workexmplecomment controller

--- app/controllers/workexample_comments_controller.rb
+++ app/controllers/workexample_comments_controller.rb
@@ -1,2 +1,32 @@
 class WorkexampleCommentsController < ApplicationController
+
+    before_action :authenticate_user!
+    before_action :set_workexample
+    access all: [:create], user: [:create], site_admin: :all    
+    
+    def create
+        @comment = WorkexampleComment.new(workexamplecomment_params)
+        @comment.user_id = current_user.id
+        respond_to do |format|
+          if @comment.save
+            format.html { redirect_to @workexample }
+          else
+            format.html { redirect_to @workexample, notice: 'Something went wrong.' }
+          end
+        end
+      end
+
+
+      private
+
+      def set_workexample
+        @blog = workexample.find(params[:workexample_comment][:workexample_id])
+      end
+  
+      # Never trust parameters from the scary internet, only allow the white list through.
+      def workexamplecomment_params
+        params.require(:workexample_comment).permit(:comment, :workexample_id)  
+      end
+
+
 end
